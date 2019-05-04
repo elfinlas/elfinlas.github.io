ba3eaee44aeacf15cf86ca409fbce0fd96c64147
MHLab <elfinlas>
2019-05-05 03:00:00 +0900

    Site updated: 2019-05-05 02:59:57

--- js/algolia-search.js
+++ js/algolia-search.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {
   var algoliaSettings = CONFIG.algolia;
-  var isAlgoliaSettingsValid = algoliaSettings.appId
+  var isAlgoliaSettingsValid = algoliaSettings.applicationID
                             && algoliaSettings.apiKey
                             && algoliaSettings.indexName;
 
@@ -12,7 +12,7 @@ $(document).ready(function() {
   }
 
   var search = instantsearch({
-    appId         : algoliaSettings.appId,
+    appId         : algoliaSettings.applicationID,
     apiKey        : algoliaSettings.apiKey,
     indexName     : algoliaSettings.indexName,
     searchFunction: function(helper) {
