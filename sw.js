7aac545a4b45891e87f1285f7eaacf86a554731a
elfinlas <elfinlas@gmail.com>
2023-01-19 01:33:28 +0900

    Updates

--- sw.js
+++ sw.js
@@ -26,10 +26,10 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-7f58d226fcd998700820.js"
+    "url": "webpack-runtime-73b529b48536ae3670c0.js"
   },
   {
-    "url": "styles.2bee05e078977d8d524f.css"
+    "url": "styles.546bf123a44435a24c87.css"
   },
   {
     "url": "styles-0ec71dd62c66cb95665c.js"
@@ -45,7 +45,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d04b8d266947be0abb5dd4628584a5f3"
+    "revision": "b2aae56f58ca25c29c31747afaf534f7"
   },
   {
     "url": "polyfill-da26d3b44af2cdb21dfe.js"
