260af8dd07c6162a1d37884758f25a747c11e843
elfinlas <elfinlas@gmail.com>
2023-06-24 02:22:33 +0900

    Updates

--- sw.js
+++ sw.js
@@ -38,14 +38,14 @@ self.__precacheManifest = [
     "url": "framework-22d51f0f5ee9f1c9c6e1.js"
   },
   {
-    "url": "app-cfa2223c2349f8700cc1.js"
+    "url": "app-bd6851c8c176ddbae127.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-44ceac2081f421b30e8f.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "87bfc9ab9cf3655d2f2720fa7b8726aa"
+    "revision": "9d3c584a962741cac26530f0fbfe4073"
   },
   {
     "url": "polyfill-da26d3b44af2cdb21dfe.js"
@@ -76,7 +76,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-cfa2223c2349f8700cc1.js`))) {
+  if (!resources || !(await caches.match(`/app-bd6851c8c176ddbae127.js`))) {
     return await fetch(event.request)
   }
 
